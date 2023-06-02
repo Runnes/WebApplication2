425022c9ae28d3e47c854bfd89af4b238bc6b6f2
Karol Workowski <charls7c@gmail.com>
2023-06-02 16:05:37 +0200

    cart site semi works

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -33,6 +33,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Cart()
+    {
+        return View();
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
